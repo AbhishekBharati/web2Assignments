9a92e7110269c1d761b69f51bcbbad619c468a1f
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-11-18 22:23:27 +0530

    completed 1st assignment of week9

--- week-9/petAdoption/src/components/Header.jsx
+++ week-9/petAdoption/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ message }) => {
   return (
-    <div>Header</div>
+    <div style={{ padding: 20, backgroundColor: "rgba(128, 71, 28, 0.5)" }}>
+      <h1>{message}</h1>
+    </div>
   )
 }
 
